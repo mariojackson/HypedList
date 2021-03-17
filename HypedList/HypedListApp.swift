02f669a7aeabfa230c3fd22bf90a4d5748291995
Mario Jackson <mario@jackson.software>
2021-03-17 14:26:54 +0100

    Initial Commit

---
+++ HypedList/HypedListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypedListApp.swift
+//  HypedList
+//
+//  Created by Mario Jackson on 17.03.21.
+//
+
+import SwiftUI
+
+@main
+struct HypedListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
