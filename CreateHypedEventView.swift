9a8c39b2b9c0de50e9d5ed15ed56200dcd78f8db
Mario Jackson <mario@jackson.software>
2021-03-17 15:24:17 +0100

    show create hyped event view

---
+++ CreateHypedEventView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateHypedEventView.swift
+//  HypedList
+//
+//  Created by Mario Jackson on 17.03.21.
+//
+
+import SwiftUI
+
+struct CreateHypedEventView: View {
+    var body: some View {
+        Text("Create HypedEvent")
+    }
+}
+
+struct CreateHypedEventView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateHypedEventView()
+    }
+}
