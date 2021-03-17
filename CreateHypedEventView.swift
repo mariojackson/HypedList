c45338858d54b7e707f24fffbc204a0f646786f0
Mario Jackson <mario@jackson.software>
2021-03-17 16:34:53 +0100

    hyped event view

--- CreateHypedEventView.swift
+++ CreateHypedEventView.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct CreateHypedEventView: View {
+    @StateObject var hypedEvent = HypedEvent();
+
     var body: some View {
-        Text("Create HypedEvent")
+        Form {
+            Section {
+                TextField("Family Vacation", text: $hypedEvent.title)
+                    .autocapitalization(.words)
+            }
+            
+            Section {
+                DatePicker("Date", selection: $hypedEvent.date, displayedComponents: [.date, .hourAndMinute])
+                    .datePickerStyle(GraphicalDatePickerStyle())
+            }
+            
+            Section {
+                ColorPicker("Color", selection: $hypedEvent.color)
+            }
+            
+            Section {
+                TextField("https://example.com", text: $hypedEvent.url)
+                    .keyboardType(.URL)
+                    .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+            }
+        }
     }
 }
 
